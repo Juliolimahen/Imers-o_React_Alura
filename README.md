b1cf3de823a7669bc6fc44c82f84c4d87e4fa4e3
Juliolimahen <juliolima_henrique@outlook.com>
2022-01-25 21:23:47 -0300

    criar pagina chat

--- README.md
+++ README.md
@@ -1,33 +1,49 @@
-# Evento Imersão React Alura
+# Imersão React Alura
 
 Desenvolvimento do AluraCord
- (Aplicação WEB simulando o funcionamento de Discord)
-
-- Documentação Next-JS
-    https://nextjs.org/docs/getting-started
+    (Aplicação WEB simulando o funcionamento de Discord)
+### Aula 1
+Documentação Next-JS
+    - https://nextjs.org/docs/getting-started
 
 Instalando dependências 
 
-- yarn init -y
+    - yarn init -y
 
-- yarn add next react-dom
+    - yarn add next react-dom
 
-- adicionar  ao package.json 
-    "scripts": {
-                "dev": "next dev",
-                "build": "next build",
-                "start": "next start",
-                "lint": "next lint"
-                }
+    - adicionar  ao package.json 
+        "scripts": {
+                    "dev": "next dev",
+                    "build": "next build",
+                    "start": "next start",
+                    "lint": "next lint"
+                    }
 
 Criando primeira Pagina
+
     - './pages/index.js'
 
 Comando para rodar aplicação em modo dev
-    yarn dev
+    - yarn dev
 
 
-- Aplicação publicada
+Publicando Aplicação 
 
-- https://cscord.vercel.app
+    - https://cscord.vercel.app
 
+### aula 2
+
+Aprofundar sobre o funcionamento: 
+
+    - useState 
+    - useRouter
+    - onChange
+    - onSubmit
+
+#### Desafio 1
+ Validação 
+    - se o campo login tiver menos de 2 caracteres
+        não mostrar a imagem e desativar o botão 
+    - senão 
+        executar normalmente 
--- coffeecord/pages/index.js
+++ coffeecord/pages/index.js
@@ -1,35 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-    );
-}
-        
 // Criando uma tag 
 function Titulo(props) {
     const Tag = props.tag;
@@ -47,32 +20,19 @@ function Titulo(props) {
         </>
     );
 }
-// Componente React
-/*function HomePage() {
-
-    // JSX (Função que retorna um HTML)
-    return (
-        <div>
-            <GlobalStyle />
-            <Titulo tag="h2">Boas vindas de volta!</Titulo>
-            <h2>Discord - Alura Matrix </h2>
-        </div>
-    )
-
-
-export default HomePage*/
 
 export default function PaginaInicial() {
-    const username = 'Juliolimahen';
+
+    const [username, setUsername] = React.useState('Juliolimahen')
+    const roteamento = useRouter();
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
                     backgroundColor: appConfig.theme.colors.primary[500],
-                    backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/counter-strike-global-offensive-dust-ii-1536x864.jpg)',
+                    backgroundImage: 'url(https://wallpaperaccess.com/full/279547.jpg)',
                     backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
                 }}
             >
@@ -91,9 +51,12 @@ export default function PaginaInicial() {
                         backgroundColor: appConfig.theme.colors.neutrals[700],
                     }}
                 >
-                    {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (infosDoEvento) {
+                            infosDoEvento.preventDefault();
+                            roteamento.push('/chat');
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -103,8 +66,17 @@ export default function PaginaInicial() {
                         <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                             {appConfig.name}
                         </Text>
-
                         <TextField
+                            value={username}
+                            onChange={
+                                function (event) {
+                                    console.log(event.target.value);
+                                    // onde esta o valor?
+                                    const valor = event.target.value;
+                                    //trocar o valor da variavel 
+                                    // atravé do rect e avisa quem tiver que avisar 
+                                    setUsername(valor);
+                                }}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
@@ -127,10 +99,6 @@ export default function PaginaInicial() {
                             }}
                         />
                     </Box>
-                    {/* Formulário */}
-
-
-                    {/* Photo Area */}
                     <Box
                         styleSheet={{
                             display: 'flex',
@@ -165,7 +133,6 @@ export default function PaginaInicial() {
                             {username}
                         </Text>
                     </Box>
-                    {/* Photo Area */}
                 </Box>
             </Box>
         </>
